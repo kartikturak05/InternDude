fe61d5191dd5cad8548755ca2e842bdf8017f8b2
Kartik Turak <kartikturak1@gmail.com>
2025-02-04 22:39:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+<link href="/dist/styles.css" rel="stylesheet"></link>
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1 class="text-3xl font-bold underline text-center">
+    Inter Dude
+  </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
